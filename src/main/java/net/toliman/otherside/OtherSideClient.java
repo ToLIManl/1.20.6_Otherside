52aa241421e575d74dd3d2fdf2b38d0038f3dd39
ToLIMan <nikita_strizhakov@bk.ru>
2024-06-13 15:39:10 +0300

    Initial commit

---
+++ src/main/java/net/toliman/otherside/OtherSideClient.java
@@ -0,0 +1,10 @@
+package net.toliman.otherside;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OtherSideClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/toliman/otherside/OtherSideDataGenerator.java
@@ -0,0 +1,11 @@
+package net.toliman.otherside;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OtherSideDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
